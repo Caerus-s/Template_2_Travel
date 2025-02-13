37a9be54bd9d0d005c5ea864a7736476c477fb07
ZenX <zenxdiscord@gmail.com>
2025-02-13 09:23:18 +0700

    feature:base

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-black font-semibold text-4xl w-screen h-screen flex justify-center items-center'>
+      <svg>
+      <path d="M125 5 L225 5 L225 200 L75 200 L75 75 Z"  />
+      </svg>
+    </div>
+  )
+}
+
+export default App
